3adec0eb6c44ca91700d7249c652b7c7b8e52935
nakozak <nicholas-kozak@uiowa.edu>
2016-09-14 11:53:05 -0500

    Update part3_spec.rb
    Fixed Part3 Spec

--- spec/part3_spec.rb
+++ spec/part3_spec.rb
@@ -5,7 +5,7 @@ describe "dessert" do
 #    lambda { Dessert.new('a', 1) }.should_not raise_error
     expect(lambda {Dessert.new('a', 1)}).not_to raise_error
   end
-  %w(:healthy? :delicious?).each do |method|
+ [:healthy?, :delicious?].each do |method|
     it "should define #{method}" do
 #      Dessert.new('a',1).should respond_to method
       expect(Dessert.new('a',1)).to respond_to(method)
@@ -18,7 +18,7 @@ describe "jellybean" do
 #    lambda { JellyBean.new('a', 1, 2) }.should_not raise_error
     expect(lambda { JellyBean.new('a', 1, 2) }).not_to raise_error
   end
-  %w(:healthy? :delicious? ).each do |method|
+    [:healthy?, :delicious?].each do |method|
     it "should define #{method}" do
 #      JellyBean.new('a',1, 2).should respond_to method 
       expect(JellyBean.new('a',1, 2)).to respond_to(method) 
